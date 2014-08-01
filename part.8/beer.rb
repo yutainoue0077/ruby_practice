e2cfe42ec8918b58d84b5d9c17e9596d6602f728
yutainoue <yutainoue009@gmail.com>
2014-08-01 12:00:11 +0900

    8章まで進行済み

---
+++ part.8/beer.rb
@@ -0,0 +1,19 @@
+class Beer
+
+  def sing(beer_count)
+    while beer_count > 0
+      puts "#{beer_count} Bottles of beer on the wall"
+      puts "#{beer_count} Bottles of beer"
+      beer_count = beer_count - 1
+      puts "Take one down and pass it around"
+      puts "#{beer_count} Bottles of beer on the wall"
+      puts ""
+    end
+  end
+
+end
+
+beer = Beer.new
+puts "何本飲みたい？？？"
+beer_count = gets.chomp.to_i
+beer.sing(beer_count)
---
+++ part.8/wed_num.rb
@@ -0,0 +1,92 @@
+def englishNumber number
+    count = 0
+    return '負でない数を入力してください.'  if number < 0
+    return 'zero' if number == 0
+
+  numString = ''
+
+  onesPlace = ['one',     'two',       'three',    'four',     'five',
+               'six',     'seven',     'eight',    'nine']
+  tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+               'sixty',   'seventy',   'eighty',   'ninety']
+  teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+               'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+
+  left  = number
+  write = left/100
+  left  = left - write*100
+
+  if write > 9999999
+    write2 = write/10000000
+    write = write - write2*10000000
+    thousand = englishNumber write2
+    numString = numString + thousand + " billion"
+    numString = numString + " and " if left > 0
+  end
+
+  if write > 9999
+    write2 = write/10000
+    write = write - write2*10000
+    thousand = englishNumber write2
+    numString = numString + thousand + " million"
+    numString = numString + " and " if left > 0
+  end
+
+  if write > 99
+    write2 = write/10
+    write = write - write2*10
+    thousand = englishNumber write2
+    numString = numString + thousand + " thousand"
+    numString = numString + " and " if left > 0
+  end
+
+  if write > 0
+    hundreds  = englishNumber write
+    numString = numString + hundreds + " hundred"
+    numString = numString + " and " if left > 0
+     if left > 0
+      numString = numString + " "
+    end
+  end
+
+  write = left/10
+  left  = left - write*10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      numString = numString + teenagers[left-1]
+      left = 0
+    else
+      numString = numString + tensPlace[write-1]
+      count = 1
+    end
+    if (left > 0) and (count == 1)
+      numString = numString + " and "
+    end
+  end
+
+  write = left
+  left  = 0
+
+  if write > 0
+    numString = numString + onesPlace[write-1]
+  end
+
+  numString
+end
+
+puts englishNumber(2)
+puts englishNumber(  9)
+puts englishNumber( 10)
+puts englishNumber( 11)
+puts englishNumber( 17)
+puts englishNumber( 32)
+puts englishNumber( 88)
+puts englishNumber( 99)
+puts englishNumber(100)
+puts englishNumber(101)
+puts englishNumber(234)
+puts englishNumber(3211)
+puts englishNumber(99999999)
+puts englishNumber(1000000000)
