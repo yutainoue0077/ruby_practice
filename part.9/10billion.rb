4da2371061b36267b6db41422792caee997441f0
yutainoue <yutainoue009@gmail.com>
2014-08-04 11:13:22 +0900

    生誕10億秒の計算を、生年月日入力式に更新

--- part.9/10billion.rb
+++ part.9/10billion.rb
@@ -1,12 +1,25 @@
 class Billion
 
-  def birthday (ten_billion)
-    b_time = Time.mktime(1989, 7, 3, 12, 0, 0)
-    b_time += ten_billion
+  def birthday (year, month, day, hour, min, sec)
+    b_time = Time.mktime(year, month, day, hour, min, sec)
+    b_time += 1000000000
     puts "あなたは#{b_time}に生誕10億秒を迎えます。"
   end
 
 end
 
 billion = Billion.new
-billion.birthday(1000000000)
+
+puts "何年生まれ？"
+year = gets.chomp.to_i
+puts "何月生まれ？"
+month = gets.chomp.to_i
+puts "何日生まれ？"
+day = gets.chomp.to_i
+puts "何時生まれ？"
+hour = gets.chomp.to_i
+puts "何分生まれ？"
+min = gets.chomp.to_i
+puts "何秒生まれ？"
+sec = gets.chomp.to_i
+billion.birthday(year, month, day, hour, min, sec)
